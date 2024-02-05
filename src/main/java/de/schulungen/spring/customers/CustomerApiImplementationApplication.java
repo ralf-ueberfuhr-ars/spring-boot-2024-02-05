441aeb4e9f26657179f014d75522796a3040d1be
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-02-05 11:07:01 +0100

    Initial commit

---
+++ src/main/java/de/schulungen/spring/customers/CustomerApiImplementationApplication.java
@@ -0,0 +1,13 @@
+package de.schulungen.spring.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApiImplementationApplication.class, args);
+	}
+
+}
