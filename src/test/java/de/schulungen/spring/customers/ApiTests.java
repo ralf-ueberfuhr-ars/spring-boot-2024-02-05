04ded4a78dbcea105d7b804ef6cb1303852b4c7f
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-02-06 16:13:02 +0100

    Extend CustomersController

--- src/test/java/de/schulungen/spring/customers/ApiTests.java
+++ src/test/java/de/schulungen/spring/customers/ApiTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -31,6 +33,38 @@ class ApiTests {
       //.andExpect(jsonPath("$.size()").value(1));
       .andExpect(jsonPath("$.size()").exists());
 
+  }
+
+  @Test
+  void shouldCreateCustomer() throws Exception {
+    String location = mvc
+      .perform(
+        post("/api/v1/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Julia",
+              "birthdate": "1996-03-04",
+              "state": "active"
+            }
+                      """
+          )
+      )
+      .andExpect(status().isCreated())
+      .andExpect(header().exists(HttpHeaders.LOCATION))
+      .andReturn()
+      .getResponse()
+      .getHeader(HttpHeaders.LOCATION);
+    // jetzt muss GET /customers/{id} auch klappen
+      assert location != null;
+      mvc.perform(
+        get(location)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name").value("Julia"))
+        .andExpect(jsonPath("$.uuid").exists());
   }
 
 
